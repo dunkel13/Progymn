49d1dd66d10dfa6f7c744de55150e7a010588412
dunkel13 <fedom9@gmail.com>
2016-02-29 23:25:52 -0500

    Update mysort.m

--- mysort.m
+++ mysort.m
@@ -3,12 +3,11 @@ function u=mysort(u)
 n=length(u);
 
 for i=1:n
-
 	for j=1:n-1
 		if u(j)>u(j+1)
 			aux=u(j);
-      u(j)=u(j+1);
-      u(j+1)=aux;
+      			u(j)=u(j+1);
+      			u(j+1)=aux;
 		else 
 			
 		end
