0d90192f9ca2fcf987d31f3af9d80492e3147f15
dunkel13 <fedom9@gmail.com>
2016-03-01 21:44:04 -0500

    Update mys.m

--- mys.m
+++ mys.m
@@ -3,15 +3,15 @@ function u=mys(u)
 n=length(u);
 
 for i=1:n
-	for j=1:n-1
-		if u(j)>u(j+1)
-			aux=u(j);
-      			u(j)=u(j+1);
-      			u(j+1)=aux;
-		else 
+for j=1:n-1
+	if u(j)>u(j+1)
+		aux=u(j);
+      		u(j)=u(j+1);
+      		u(j+1)=aux;
+	else 
 			
-		end
 	end
+end
 end
 u
 end
