6b027daaedeeb4444567fb43b6841f173db8f880
dunkel13 <fedom9@gmail.com>
2016-03-14 15:52:43 -0500

    Create biseccion.m

---
+++ biseccion.m
@@ -0,0 +1,37 @@
+a=0;
+b=2;
+
+fa=fbp(a);
+fb=fbp(b);
+
+% numero maximo de iteraciones
+
+M=100;
+% tolerancia residuo
+tolr=0.00000001;
+% tolerancia incremento
+toli= 0.00001;
+lon=b-a;
+for i=1:M
+	lon=lon/2;
+	c=a+lon;
+	fprintf('c(%d) = %.10f \n',i,c)
+	
+	fc=fbp(c);
+	if (abs(fc)<tolr)
+		r=c;
+		break;
+	else
+%		if (fa*fc<0)
+		if (sign(fa)~=sign(fc))
+			b=c;	
+			fb=fc;
+		else
+			a=c;
+			fa=fc;
+		end
+	end
+%	if ((lon/2)<toli)
+%		r=c;
+%		break;
+end
