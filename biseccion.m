5b332df919c86ca27504c32366b6c2d93903bf5a
dunkel13 <fedom9@gmail.com>
2016-03-16 13:14:49 -0500

    Create biseccion.m

---
+++ biseccion.m
@@ -0,0 +1,37 @@
+% este programa aproxima la raiz de f(x)=0
+% con f programada en fparabiseccion.m
+% parametros de entrada:
+%		a,b  : extremos intervalo inical
+%		Nmax : Numero maximo de iteraciones
+%		tolin: tolerancia para distancia ultimo intervalo
+% 		tolres: toleracia para el residuo
+function biseccion(a,b,Nmax,tolint, tolres)
+
+fa=fparabiseccion(a);
+fb=fparabiseccion(b);
+e=b-a;
+
+if ( sign(fa) == sign(fb) ) 
+	fprintf( ' Error: no hay cambio de signo\n');
+else
+for k=1:Nmax
+	e=e/2;
+	c=a+e;
+	fc=fparabiseccion(c);
+	if( abs(e) < tolint || abs(fc)<tolres )
+		break;
+	end
+	
+	if( sign(fc)~= sign(fa) )
+		b=c;
+		fb=fc;
+	else
+		a=c;
+		fa=fc;
+	end
+end
+
+end
+
+
+fprintf( ' \n Raiz aproximada r=%.10f \n', c) 
