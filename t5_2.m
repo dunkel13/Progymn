c69684b740ceffc4cf995c8eaa5e1662888cb551
dunkel13 <fedom9@gmail.com>
2016-04-08 23:46:49 -0500

    Update t5_2.m

--- t5_2.m
+++ t5_2.m
@@ -9,17 +9,14 @@ for i=1:4
 D(i)=newton(z(i),Nmax,tolinc,tolres);
 end
 
-for i=-10:0.1:10
-x0=i
+for x0=-10:0.1:10
 f=fparanewton(x0);
-n=0;
 if ( abs(f) < tolres) 
 	r=x0;
 else
 	for k=1:Nmax
 		fp=fprimaparanewton(x0);
 		x1=x0-f./fp;
-    n=n+1;
     u(k)=x0;
 		f=fparanewton(x1);
     
@@ -32,16 +29,20 @@ else
     fprintf('La iteracion %d es %.10f \n',k,x0);
 	end
   u
-  if(x1==D(1))
+  if(x0==D(1))
   plot(u,u*0,'-r*')
-  elseif(x1==D(2))
+  end
+  hold on
+  if(x0==D(2))
   plot(u,u*0,'-b*')
-  elseif(x1==D(3))
+  end
+  hold on
+  if(x0==D(3))
   plot(u,u*0,'-g*')
-  elseif(x1==D(4))
+  end
+  hold on
+  if(x0==D(4))
   plot(u,u*0,'-y*')
-  elseif(x1~=D(1) && x1~=D(2) && x1~=D(3) && x1~=D(4))
-  plot(u,u*0,'-k*')
   end
 end
-end	
+end			
