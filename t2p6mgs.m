a3300a7d1b84a8dc407d54bf70aba7d1421393f0
dunkel13 <fedom9@gmail.com>
2016-04-20 11:11:18 -0500

    Update t2p6mgs.m

--- t2p6mgs.m
+++ t2p6mgs.m
@@ -1,49 +1,48 @@
 for a=-10:0.1:10
-N=3;
+
+N=4;
 
 A1=diag(ones(N,1));
-A2=diag(ones(N-2,1),2);
-A3=diag(ones(N-2,1),-2);
-% en esta parte es 'a' que son los valores de la diagonal
-A=a*A1+A2+A3;
+A2=diag(ones(N-3,1),3);
+A3=diag(ones(N-3,1),-3);
 
+A=a*A1+A2+A3;
+% los valores de a para los cuales Gauss-Siedel converge son
+% a=[-10:-1.1] && [1.1:10]
 b=ones(N,1);
 
 Nmax=10000;
 
-tolres=0.0000001;
-tolinc=0.0000001;
+tolres=0.000001;
+tolinc=0.00001;
 
 x0=0*b;
 
-D=diag(diag(A));
-L=-tril(A);
-
-Mgauss=D-L;
+Mjac=diag(diag(A));
 
 for k=1:Nmax
 
 	%residuo
 	r=b-A*x0;
 
-	% resuelvo sistema como M de Guass-Siedel
-	delta=Mgauss\r;
-
-	%fprintf( ' norm inc(%d) =%.10f\n',k, norm(delta))
+	% resuelvo sistema como M; JACOBBI
+	delta= Mjac\ r;
+  % calculo nuevo iterado
+	x1=x0+delta;
+	
+	%fprintf( ' norm inc(%d) =%.10f\n',k, norm(delta));
 
 	if(norm(r)<tolres)
-		fprintf(' paso (%d), valor de a: %.10f \n',k,a)
+		fprintf('pasos:%d con a=%d \n',k,a)
+    hold on
 		break;
 	end
-
-	% calculo nuevo iterado
-	x1=x0+delta;
 	
 	%if( norm(delta)<tolinc)
 	%	break;
 	%end
-
-%actualiza para continuar iteracion
+		
+%actualiza para continuar}
 x0=x1;
 end
 end
